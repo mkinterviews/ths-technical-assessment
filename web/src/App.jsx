ddbe91f781344b4759897d7f43d943d14f8955b4
Mat Kastner <mat@Mats-Air.broadband>
2025-10-05 17:33:50 +0100

    add a container to prevent pet items from stretching too far

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import PetList from './components/PetList/';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import PetList from "./components/PetList/";
+import "./App.css";
 
 const App = () => (
-  <div className="App">
+  <div className="App container">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
     </header>
-      <div className="App-content">
-        <PetList data-testid="pet-list" />
-      </div>
+    <main className="App-content">
+      <PetList data-testid="pet-list" />
+    </main>
   </div>
 );
 

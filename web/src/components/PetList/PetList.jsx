ad9d0ef72c74d52c2300df82a21f228a068af16e
Mat Kastner <mat@Mats-Air.broadband>
2025-10-05 15:49:29 +0100

    add basic SWR request

--- web/src/components/PetList/PetList.jsx
+++ web/src/components/PetList/PetList.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import useSWR from "swr";
+
+import PetItem from "./components/PetItem";
+
 import "./PetList.css";
 
 const PetList = () => {
+  const { data } = useSWR("/api/pets", async (key) => {
+    const response = await fetch(key);
+    return await response.json();
+  });
+
   return (
     <>
       <h1 className="Pets-title">My Pets</h1>
+      {data?.map((pet) => {
+        return <PetItem key={pet.id} pet={pet} />;
+      })}
     </>
   );
 };

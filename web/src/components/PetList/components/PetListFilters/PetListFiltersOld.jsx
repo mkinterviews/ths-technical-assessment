7dc6063be858df69432a3e7af4cb9cc54ad79d4a
Mat Kastner <mat@Mats-Air.broadband>
2025-10-05 19:33:27 +0100

    rename pet list filters

---
+++ web/src/components/PetList/components/PetListFilters/PetListFiltersOld.jsx
@@ -0,0 +1,27 @@
+import { PET_TYPES } from "../../../../mocks/generatePets";
+
+export const PetListFilters = ({ filters, setFilters }) => {
+  return (
+    <div>
+      <input
+        aria-label="Search"
+        value={filters["q"]}
+        onChange={(e) =>
+          setFilters((filters) => ({ ...filters, q: e.target.value }))
+        }
+      />
+      <select
+        aria-label="Type"
+        value={filters["type"]}
+        onChange={(e) =>
+          setFilters((filters) => ({ ...filters, type: e.target.value }))
+        }
+      >
+        <option value="">All types</option>
+        {PET_TYPES.map((type) => (
+          <option>{type}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
